350d07bffab76a2bb3b0ffed9d22d382e9151167
Trent Rand <trand@squarespace.com>
2019-05-15 17:45:50 -0400

    Add 'click to copy branch name' script for bitbucket

---
+++ bitbucket/BitBucket Click to Copy Branch Name.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         BitBucket Click to Copy Branch Name
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Copy branch name to clipboard when clicking it within BitBucket PRs
+// @author       Trent Rand
+// @match        https://code.squarespace.net/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const branchNameLabel = document.querySelector('.ref-name-from > .branch-name');
+
+    branchNameLabel.addEventListener('click', function(event) {
+        const branchName = event.target.textContent;
+        navigator.clipboard.writeText(branchName).then(function() {
+            console.log('Copied to clipboard!');
+            branchNameLabel.style.backgroundColor = "yellow";
+            branchNameLabel.textContent = "Copied!".padEnd(branchName.length);
+            setTimeout(function() {
+                branchNameLabel.textContent = branchName;
+                branchNameLabel.style.backgroundColor = "unset";
+            }, 500);
+        });
+    });
+})();
